f53beb6380c0b6db3338ba1f5182669b47a5885a
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2023-11-23 23:41:34 +0100

    update README.md

--- src/main/java/com/example/youquiz/YouQuizApplication.java
+++ src/main/java/com/example/youquiz/YouQuizApplication.java
@@ -2,10 +2,8 @@ package com.example.youquiz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-
 public class YouQuizApplication {
 
     public static void main(String[] args) {
