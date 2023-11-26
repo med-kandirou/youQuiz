487d3e05da2db3753fedac39143cdfdcfb7cbd3f
kandirou mohamed <105118673+med-kandirou@users.noreply.github.com>
2023-11-26 14:40:52 +0100

    update

--- src/main/java/com/example/youquiz/helpers/WebConfig.java
+++ src/main/java/com/example/youquiz/helpers/WebConfig.java
@@ -12,6 +12,5 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
-
     }
 }
